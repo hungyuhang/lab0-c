222279845dad26639b307f05e73b877e5c1730c9
hungyuhang <345genius@gmail.com>
2024-02-28 22:34:18 +0800

    Implement q_reverseK
    Use brute-force to solve this problem.

--- queue.c
+++ queue.c
@@ -190,6 +190,36 @@ void q_reverse(struct list_head *head)
 void q_reverseK(struct list_head *head, int k)
 {
     // https://leetcode.com/problems/reverse-nodes-in-k-group/
+    if (q_size(head) < k)
+        return;
+    struct list_head *front = head->next;
+    struct list_head *curr;
+    struct list_head *rear;
+    struct list_head *temp;
+    int cnt = 1;
+    for (rear = head->next; rear != head; rear = rear->next) {
+        if (cnt == k) {
+            curr = front;
+            while (true) {
+                temp = curr->next;
+                curr->next = curr->prev;
+                curr->prev = temp;
+                if (curr == rear)
+                    break;
+                curr = curr->prev;
+            }
+            front->next->next = rear;
+            rear->prev->prev = front;
+            temp = front->next;
+            front->next = rear->prev;
+            rear->prev = temp;
+            cnt = 0;
+            rear = front;
+            front = front->next;
+        }
+        cnt++;
+    }
+    return;
 }
 
 /* Sort elements of queue in ascending/descending order */
