f38e47accd9544ba54018414af9c6598b009df37
hungyuhang <345genius@gmail.com>
2024-03-03 23:22:27 +0800

    Fix issues of shuffle function
    1. Delete function call of srand()
      calling this function will make the distribution of the output not
      uniform, so delete it.
    2. Fix false logic of the function.

--- q_shuffle.c
+++ q_shuffle.c
@@ -8,18 +8,15 @@
 /* Shuffle queue using Fisher–Yates shuffle */
 void q_shuffle(struct list_head *head)
 {
-    srand(time(NULL));
     int range = q_size(head);
     struct list_head *tmp;
     while (range) {
         int cnt = rand() % range;
-        if (cnt != 0) {
-            tmp = head;
-            while (cnt--) {
-                tmp = tmp->next;
-            }
-            list_move_tail(tmp, head);
+        tmp = head->next;
+        while (cnt--) {
+            tmp = tmp->next;
         }
+        list_move_tail(tmp, head);
         range--;
     }
     return;
